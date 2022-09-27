72a3258fcb10405bf9fa7120ade69b5fdb6fc566
Kiryl Litoshka <kovzik08@gmail.com>
2022-09-28 00:19:31 +0300

    extended some dialog options and fix echo response for non registered messages (in progress)

--- main.py
+++ main.py
@@ -17,7 +17,38 @@ options = {
         1: {
             "type": "text",
             "message": {
-                "text": "Confirmed"
+                "text": "Отлично, ты ответила. Мой босс возлагал большие надежды именно на тебя.",
+                "keyboard": [
+                    {
+                        "text": "Босс..?"
+                    }
+                ]
+            },
+            1: {
+                "type": "text",
+                "message": {
+                    "text": "Босс..?",
+                    "keyboard": [
+                        {
+                            "text": "Это шутка какая-то или новый вид подката?"
+                        },
+                        {
+                            "text": "Он что, Бэтмен? Я переписываюсь с дворецкиим?"
+                        }
+                    ]
+                },
+                1: {
+                    "type": "text",
+                    "message": {
+                        "text": "Variant 1"
+                    }
+                },
+                2: {
+                    "type": "text",
+                    "message": {
+                        "text": "Variant 2"
+                    }
+                }
             }
         }
     }
@@ -42,14 +73,25 @@ async def main(message: types.Message):
 @dispatcher.message_handler()
 async def echo_check(message: types.Message):
     user = types.User.get_current()
-    if message.text not in users[user.id]["registered_messages"]:
-        print("message not in pool")  # test print if message not in user pool (need replace to Exception)
-        await bot.delete_message(message.chat.id, message.message_id)
-    next_index = users[user.id]["registered_messages"].index(message.text) + 1
-    if not next_index:
-        raise
-    users[user.id]["option_choices"].append(next_index)
-    user_choice_sequence = users[message.from_user.id]["option_choices"]
+    try:
+        if not users[user.id]:
+            users[user.id] = {}
+        users[user.id] = {
+            "registered_messages": users[user.id].get("registered_messages", []),
+            "option_choices": users[user.id].get("option_choices", [])
+        }
+        if message.text not in users[user.id]["registered_messages"]:
+            # test print if message not in user pool (need replace to Exception)
+            print("message not in pool")
+            await bot.delete_message(message.chat.id, message.message_id)
+        else:
+            next_index = users[user.id]["registered_messages"].index(
+                message.text) + 1
+            users[user.id]["option_choices"].append(next_index)
+    except KeyError:
+        print("KEY ERROR")
+    user_choice_sequence = [1] if not users[user.id].get(
+        "option_choices") else users[user.id]["option_choices"]
     msg_option = get_option_sequence(user_choice_sequence)
     await send_message(message.from_user.id, msg_option)
 
@@ -78,7 +120,8 @@ def build_keyboard(args: dict):
     keyboard_args = args.get("keyboard", None)
     if keyboard_args is None:
         return None
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+    keyboard = types.ReplyKeyboardMarkup(
+        resize_keyboard=True, one_time_keyboard=True)
     register_user_handle(keyboard_args)
     for keyboard_item in keyboard_args:
         keyboard.add(types.KeyboardButton(keyboard_item["text"]))
@@ -100,5 +143,19 @@ def get_option_sequence(indexes_list: list):
     return result
 
 
+async def on_shutdown(dp: Dispatcher):
+    print("CLOSED")
+    dp.stop_polling()
+    await dp.wait_closed()
+
+
+async def on_startup(args):
+    """
+        Some startup actions
+    """
+    print(args)
+
+
 if __name__ == "__main__":
-    executor.start_polling(dispatcher, skip_updates=False)
+    executor.start_polling(dispatcher, skip_updates=False,
+                           on_shutdown=on_shutdown, on_startup=on_startup)
