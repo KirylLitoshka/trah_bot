255559d0c9561f4ed635de34f74432a11b220c07
Kirill Litoshko <kovzik08@gmail.com>
2022-09-30 17:10:34 +0300

    updated handlers

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,43 +1,39 @@
-from aiogram.types import Message, User
+from aiogram.types import Message
 from aiogram import Dispatcher, Bot
 from utils import send_message
-
-
 
 
 async def start(message: Message):
     users = Dispatcher.get_current().data["users"]
     options = Dispatcher.get_current().data["conversation"]
-    user_id = message.from_user.id
-    if user_id not in users:
-        users[user_id] = {"option_choiced": "1"}
+    user_id = str(message.from_user.id)
+    if user_id not in users or "last_choiced_option" not in users[user_id]:
+        users[user_id] = {"last_choiced_option": "1"}
         await message.answer(text="У Вас есть новое совпадение")
-        await send_message(message, options[users[user_id]["option_choiced"]])
-
+    await send_message(message, options[users[user_id]["last_choiced_option"]])
 
 
 async def echo(message: Message):
-    # users = Dispatcher.get_current().data["users"]
-    # user_id = message.from_user.id
-    # try:
-    #     if not users[user_id]:
-    #         users[user_id] = {}
-    #     users[user.id] = {
-    #         "registered_messages": users[user.id].get("registered_messages", []),
-    #         "option_choices": users[user.id].get("option_choices", [])
-    #     }
-    #     if message.text not in users[user.id]["registered_messages"]:
-    #         # test print if message not in user pool (need replace to Exception)
-    #         print("message not in pool")
-    #         await bot.delete_message(message.chat.id, message.message_id)
-    #     else:
-    #         next_index = users[user.id]["registered_messages"].index(
-    #             message.text) + 1
-    #         users[user.id]["option_choices"].append(next_index)
-    # except KeyError:
-    #     print("KEY ERROR")
-    # user_choice_sequence = [1] if not users[user.id].get(
-    #     "option_choices") else users[user.id]["option_choices"]
-    # msg_option = get_option_sequence(user_choice_sequence)
-    # await send_message(message.from_user.id, msg_option)
-    await message.answer(message.text)
+    dispatcher = Dispatcher.get_current()
+    users = dispatcher.data["users"]
+    options = dispatcher.data["conversation"]
+    user_id = str(message.from_user.id)
+    bot = Bot.get_current()
+    if user_id not in users:
+        users[user_id] = {}
+        await start(message)
+        await bot.delete_message(message.chat.id, message.message_id)
+        return
+    users[user_id] = {
+        "registered_messages": users[user_id].get("registered_messages", []),
+        "last_choiced_option": users[user_id].get("last_choiced_option", "1"),
+    }
+    if message.text not in users[user_id]["registered_messages"]:
+        # test print if message not in user pool (need replace to Exception)
+        await bot.delete_message(message.chat.id, message.message_id)
+        return
+    else:
+        choice_index = users[user_id]["registered_messages"].index(message.text)
+        users[user_id]["last_choiced_option"] = users[user_id]["registered_answers_id"][choice_index]
+    await send_message(message, options[users[user_id]["last_choiced_option"]])
+    # await message.answer(message.text)
