88af44db0afa2d65394544019753b808591fb2f8
Kirill Litoshko <kovzik08@gmail.com>
2022-10-03 17:07:53 +0300

    added sending message with delay

--- bot/utils.py
+++ bot/utils.py
@@ -1,31 +1,34 @@
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, InputFile, User
 from aiogram import Dispatcher
 from setting import IMAGES_DIR
+import asyncio
+
 
 async def send_message(message: Message, msg_option: dict):
     message_args = msg_option["message"]
     message_type = msg_option["type"]
+    await asyncio.sleep(message_args.get("delay", 0.5))
     if message_type == "text":
         await message.answer(
-            text=message_args["text"],
-            reply_markup=build_keyboard(message_args)
+            text=message_args["text"], reply_markup=build_keyboard(message_args)
         )
     elif message_type == "photo":
         await message.answer_photo(
             photo=InputFile(f'{IMAGES_DIR}/{message_args["photo"]}'),
             caption=message_args["text"],
-            reply_markup=build_keyboard(message_args)
+            reply_markup=build_keyboard(message_args),
         )
     else:
         print("CHECK MESSAGE TYPE!")  # replace with Exception
+    if "delayed_message" in msg_option:
+        await send_message(message, msg_option["delayed_message"])
 
 
 def build_keyboard(args: dict):
     keyboard_args = args.get("keyboard", None)
     if keyboard_args is None:
         return None
-    keyboard = ReplyKeyboardMarkup(
-        resize_keyboard=True, one_time_keyboard=True)
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     register_user_choices(keyboard_args)
     for keyboard_item in keyboard_args:
         keyboard.add(KeyboardButton(keyboard_item["text"]))
