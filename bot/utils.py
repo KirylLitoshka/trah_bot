33941be23d499314a45993a4299a9fd01079e048
Kirill Litoshko <kovzik08@gmail.com>
2022-10-06 16:57:05 +0300

    added sticker answer and updated keyboard builder when need split answers by relationship param

--- bot/utils.py
+++ bot/utils.py
@@ -18,6 +18,11 @@ async def send_message(message: Message, msg_option: dict):
             caption=message_args["text"],
             reply_markup=build_keyboard(message_args),
         )
+    elif message_type == "sticker":
+        await message.answer_sticker(
+            sticker=message_args["sticker_id"],
+            reply_markup=build_keyboard(message_args)
+        )
     else:
         print("CHECK MESSAGE TYPE!")  # replace with Exception
     if "delayed_message" in msg_option:
@@ -29,7 +34,7 @@ def build_keyboard(args: dict):
     check_relationship = args["check_relationship"]
     if keyboard_args is None:
         return
-    register_user_choices(check_relationship, keyboard_args)
+    keyboard_args = register_user_choices(check_relationship, keyboard_args)
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
     for keyboard_item in keyboard_args:
         keyboard.add(KeyboardButton(keyboard_item["text"]))
@@ -43,3 +48,4 @@ def register_user_choices(check_relationship: bool, keyboard_data: list):
         keyboard_data = keyboard_data[1:] if users[user_id]["relationship"] >= 0 else keyboard_data[:1]
     users[user_id]["registered_messages"] = [x["text"] for x in keyboard_data]
     users[user_id]["registered_answers_id"] = [x["answer_id"] for x in keyboard_data]
+    return keyboard_data
