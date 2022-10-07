032dd04d0312c587abbd8610419cf96faba91ac2
Kirill Litoshko <kovzik08@gmail.com>
2022-10-07 16:57:19 +0300

    added handler for returning to root bot

--- bot/handlers.py
+++ bot/handlers.py
@@ -1,9 +1,15 @@
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    MenuButtonCommands,
+    InlineKeyboardMarkup,
+    InlineKeyboardButton,
+)
 from aiogram import Dispatcher, Bot
 from utils import send_message
 
 
 async def start(message: Message):
+    await Bot.get_current().set_chat_menu_button(message.chat.id, MenuButtonCommands())
     users = Dispatcher.get_current().data["users"]
     options = Dispatcher.get_current().data["conversation"]
     user_id = str(message.from_user.id)
@@ -11,6 +17,24 @@ async def start(message: Message):
         users[user_id] = {"last_choiced_option": "1"}
         await message.answer(text="У Вас есть новое совпадение")
     await send_message(message, options[users[user_id]["last_choiced_option"]])
+
+
+async def back(message: Message):
+    inline_keyboard = InlineKeyboardMarkup(
+        row_width=1,
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Тыц",
+                    url="",  # link to root bot
+                )
+            ]
+        ],
+    )
+    await message.answer(
+        text="Для перехода в основной канал нажмите кнопку ниже",
+        reply_markup=inline_keyboard,
+    )
 
 
 async def echo(message: Message):
@@ -35,9 +59,10 @@ async def echo(message: Message):
         return
     else:
         choice_index = users[user_id]["registered_messages"].index(message.text)
-        if len(users[user_id]["registered_messages"]) > 1 :
+        if len(users[user_id]["registered_messages"]) > 1:
             relation_choice_value = -1 if choice_index == 0 else 1
             users[user_id]["relationship"] += relation_choice_value
-        users[user_id]["last_choiced_option"] = users[user_id]["registered_answers_id"][choice_index]
+        users[user_id]["last_choiced_option"] = users[user_id]["registered_answers_id"][
+            choice_index
+        ]
     await send_message(message, options[users[user_id]["last_choiced_option"]])
-
