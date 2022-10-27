ff31cb5c215c2f13ecf2b5123c4f1e3dfd9425b9
Kirill Litoshko <kirill.litoshko@gmail.com>
2022-10-27 23:12:27 +0300

    cleaned useless stuff

--- bot/utils/keyboard.py
+++ bot/utils/keyboard.py
@@ -5,9 +5,7 @@ from utils.choices import check_args
 
 def reply_keyboard(user, message_args):
     dialogs = Dispatcher.get_current().data["dialogs"]
-    on_choice = None
     if not message_args["choices"]:
-        user["registered_answers"] = []
         return ReplyKeyboardRemove()
     else:
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
@@ -19,19 +17,14 @@ def reply_keyboard(user, message_args):
                     continue
             choice_message = dialogs[choice]
             keyboard.add(KeyboardButton(choice_message["text"]))
-            next_id = (
-                choice_message["jump_id"]
-                if choice_message["jump_id"]
-                else str(int(choice) + 1)
-            )
+            next_id = choice_message.get("jump_id") or str(int(choice) + 1)
+            on_choice = None
             if message_args["choices_param"]:
                 on_choice = message_args["choices_param"][index]["on_choice"]
-            user_answers.append(
-                {
-                    "text": choice_message["text"],
-                    "next_id": next_id,
-                    "on_choice": on_choice,
-                }
-            )
+            user_answers.append({
+                "text": choice_message["text"],
+                "next_id": next_id,
+                "on_choice": on_choice,
+            })
         user["registered_answers"] = user_answers
         return keyboard
