ea47c27177b3a1fd550ce2773a478fe54b9c5e00
Kirill Litoshko <kirill.litoshko@gmail.com>
2022-12-20 17:01:28 +0300

    added minho bot

---
+++ bot_minho/setting.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).parent
+STATIC_DIR = os.path.join(BASE_DIR, "static")
+
+STORAGE_DIRS = {
+    "STORAGE_DIR": os.path.join(BASE_DIR, "storage"),
+    "MEDIA_DIR": os.path.join(STATIC_DIR, "media"),
+    "IMAGES_DIR": os.path.join(STATIC_DIR, "img")
+}
+
+DEFAULT_USER_MODEL = {
+    "last_received_message_id": None,
+    "tender": 0,
+    "registered_answers": [{"text": "/start", "next_id": "0", "on_choice": None}],
+}
