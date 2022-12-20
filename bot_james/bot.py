9434732c0ba3cb8b2d920067b8695fa57117d6cc
Kirill Litoshko <kirill.litoshko@gmail.com>
2022-12-20 17:02:12 +0300

    added james bot

---
+++ bot_james/bot.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, executor
+
+from base.utils.utils import on_shutdown, on_startup
+from bot_james.setting import DEFAULT_USER_MODEL, STORAGE_DIRS
+
+
+def main():
+    bot = Bot("")
+    dispatcher = Dispatcher(bot)
+    executor.start_polling(
+        dispatcher=dispatcher,
+        on_startup=on_startup(STORAGE_DIRS, DEFAULT_USER_MODEL),
+        on_shutdown=on_shutdown,
+        skip_updates=True
+    )
+
+
+if __name__ == "__main__":
+    main()
